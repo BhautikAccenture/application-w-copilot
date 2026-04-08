b44c75d01cc48eb0bb47e949c65f2ddf54d049ca
BhautikAccenture <bhautik.shah@accenture.com>
2026-04-08 13:37:19 +0000

    fixed issues

--- octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -1,7 +1,6 @@
 from django.core.management.base import BaseCommand
-from django.contrib.auth import get_user_model
 from django.db import connection
-from djongo import models as djongo_models
+from octofit_tracker.models import User, Team, Activity, Leaderboard, Workout
 
 
 class Command(BaseCommand):
@@ -9,63 +8,39 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         self.stdout.write(self.style.SUCCESS('Deleting old data...'))
-        # Drop all collections (both old and new names)
-        with connection.cursor() as cursor:
-            for collection in ['users', 'teams', 'activities', 'leaderboard', 'workouts',
-                             'octofit_tracker_user', 'octofit_tracker_team', 
-                             'octofit_tracker_activity', 'octofit_tracker_leaderboard',
-                             'octofit_tracker_workout']:
-                try:
-                    cursor.db_conn.drop_collection(collection)
-                except:
-                    pass
+        # Clear all collections
+        User.objects.all().delete()
+        Team.objects.all().delete()
+        Activity.objects.all().delete()
+        Leaderboard.objects.all().delete()
+        Workout.objects.all().delete()
 
         self.stdout.write(self.style.SUCCESS('Inserting test data...'))
+        
         # Users
-        users = [
-            {'name': 'Tony Stark', 'email': 'ironman@marvel.com', 'team': 'marvel'},
-            {'name': 'Steve Rogers', 'email': 'cap@marvel.com', 'team': 'marvel'},
-            {'name': 'Bruce Wayne', 'email': 'batman@dc.com', 'team': 'dc'},
-            {'name': 'Clark Kent', 'email': 'superman@dc.com', 'team': 'dc'},
-        ]
-        with connection.cursor() as cursor:
-            cursor.db_conn['users'].insert_many(users)
-            cursor.db_conn['users'].create_index('email', unique=True)
+        User.objects.create(name='Tony Stark', email='ironman@marvel.com', team='marvel')
+        User.objects.create(name='Steve Rogers', email='cap@marvel.com', team='marvel')
+        User.objects.create(name='Bruce Wayne', email='batman@dc.com', team='dc')
+        User.objects.create(name='Clark Kent', email='superman@dc.com', team='dc')
 
         # Teams
-        teams = [
-            {'name': 'marvel', 'members': ['ironman@marvel.com', 'cap@marvel.com']},
-            {'name': 'dc', 'members': ['batman@dc.com', 'superman@dc.com']},
-        ]
-        with connection.cursor() as cursor:
-            cursor.db_conn['teams'].insert_many(teams)
+        Team.objects.create(name='marvel', members=['ironman@marvel.com', 'cap@marvel.com'])
+        Team.objects.create(name='dc', members=['batman@dc.com', 'superman@dc.com'])
 
         # Activities
-        activities = [
-            {'user_email': 'ironman@marvel.com', 'activity': 'Running', 'duration': 30},
-            {'user_email': 'cap@marvel.com', 'activity': 'Cycling', 'duration': 45},
-            {'user_email': 'batman@dc.com', 'activity': 'Swimming', 'duration': 25},
-            {'user_email': 'superman@dc.com', 'activity': 'Flying', 'duration': 60},
-        ]
-        with connection.cursor() as cursor:
-            cursor.db_conn['activities'].insert_many(activities)
+        Activity.objects.create(user_email='ironman@marvel.com', activity='Running', duration=30)
+        Activity.objects.create(user_email='cap@marvel.com', activity='Cycling', duration=45)
+        Activity.objects.create(user_email='batman@dc.com', activity='Swimming', duration=25)
+        Activity.objects.create(user_email='superman@dc.com', activity='Flying', duration=60)
 
         # Leaderboard
-        leaderboard = [
-            {'team': 'marvel', 'points': 75},
-            {'team': 'dc', 'points': 85},
-        ]
-        with connection.cursor() as cursor:
-            cursor.db_conn['leaderboard'].insert_many(leaderboard)
+        Leaderboard.objects.create(team='marvel', points=75)
+        Leaderboard.objects.create(team='dc', points=85)
 
         # Workouts
-        workouts = [
-            {'user_email': 'ironman@marvel.com', 'workout': 'Pushups', 'reps': 100},
-            {'user_email': 'cap@marvel.com', 'workout': 'Situps', 'reps': 150},
-            {'user_email': 'batman@dc.com', 'workout': 'Pullups', 'reps': 80},
-            {'user_email': 'superman@dc.com', 'workout': 'Squats', 'reps': 200},
-        ]
-        with connection.cursor() as cursor:
-            cursor.db_conn['workouts'].insert_many(workouts)
+        Workout.objects.create(user_email='ironman@marvel.com', workout='Pushups', reps=100)
+        Workout.objects.create(user_email='cap@marvel.com', workout='Situps', reps=150)
+        Workout.objects.create(user_email='batman@dc.com', workout='Pullups', reps=80)
+        Workout.objects.create(user_email='superman@dc.com', workout='Squats', reps=200)
 
         self.stdout.write(self.style.SUCCESS('Database populated with test data!'))
